60d75dadb5c5eeb5aea070e6f8bc432c92451aeb
olamideakintayo <divineolamide977@gmail.com>
2022-11-17 07:17:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text = "Learn React" />
+      <Todo  text = "Master React" />
+      <Todo  text = "Explore React" />
+    </div>
+  );
+}
+
+export default App;
+ 
